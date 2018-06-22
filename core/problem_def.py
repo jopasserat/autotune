5a815ad143f6579196573ec09947b8c8e7e5bd96
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2018-06-22 17:21:11 +0100

    enh: store current arm and param domain as member variables

--- core/problem_def.py
+++ core/problem_def.py
@@ -1,10 +1,17 @@
 import pprint
 import abc
 
+from collections import OrderedDict
 
 class Problem(object):
 
     __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        # make sure keys are retrieved in same order for pair parameters
+        self.domain = OrderedDict()
+        # FIXME awful trick to retrieve generated values from PairParams given current design
+        self.current_arm = {}
 
     def generate_arms(self, n, hps=None):
         arms = []
@@ -15,18 +22,18 @@ class Problem(object):
     def generate_random_arm(self, hps=None):
         if not hps:
             hps = self.domain.keys()
-        arm = {}
+        self.current_arm.clear()
         for hp in self.domain.keys():
-            # if sample is required, draw for param range
+            # if sample is required, draw from param range
             if hp in hps:
                 val = self.domain[hp].get_param_range(1, stochastic=True)
-                arm[hp] = val[0]
+                self.current_arm[hp] = val[0]
             # else set to default
             else:
                 val = self.domain[hp].init_val
                 assert val is not None, "No default value is set for param {}".format(hp)
-                arm[hp] = val
-        return arm
+                self.current_arm[hp] = val
+        return self.current_arm.copy()
 
     def print_domain(self):
         pp = pprint.PrettyPrinter(indent=4)
@@ -39,4 +46,3 @@ class Problem(object):
     @abc.abstractmethod
     def initialise_domain(self):
         pass
-
